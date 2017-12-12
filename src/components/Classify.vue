785f0ba2704821457e1abfe04a3b497cd437044a
51942664 <51942664@qq.com>
2017-12-12 11:29:19 +0800

    新增主页

---
+++ src/components/Classify.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="classify">
+    {{msg}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Classify',
+  data () {
+    return {
+      msg: 'classify'
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
