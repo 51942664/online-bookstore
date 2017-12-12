1cfc0d76048db018ffe5f49a6c24bc361d3c7781
51942664 <51942664@qq.com>
2017-12-12 11:30:29 +0800

    新增个人中心

---
+++ src/components/Self.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="self">
+    {{msg}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Self',
+  data () {
+    return {
+      msg: 'self'
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
