4f953381362c93b50765e92ce6109e43244d69c7
51942664 <51942664@qq.com>
2017-12-13 00:01:11 +0800

    主页组件

---
+++ src/components/BookList.vue
@@ -0,0 +1,53 @@
+<template>
+<div class="book">
+        <div>
+            <h3>新书上架</h3>
+            <ul>
+                <li v-for="booklist in newbook" :key="booklist.id">
+                    <div>
+                        <img :src="booklist.img">
+                        <p class="name">{{booklist.name}}</p>
+                        <p class="price">￥{{booklist.price}}</p>
+                    </div>
+                </li>
+            </ul>
+        </div>
+        <div>
+            <h3>编辑推荐</h3>
+            <ul>
+                <li v-for="booklist in eidtbook" :key="booklist.id">
+                    <div>
+                        <img :src="booklist.img">
+                        <p class="name">{{booklist.name}}</p>
+                        <p class="price">￥{{booklist.price}}</p>
+                    </div>
+                </li>
+            </ul>
+        </div>
+        <div>
+            <h3>热门新书</h3>
+            <ul>
+                <li v-for="booklist in hotbook" :key="booklist.id">
+                    <div>
+                        <img :src="booklist.img">
+                        <p class="name">{{booklist.name}}</p>
+                        <p class="price">￥{{booklist.price}}</p>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        name: "BookList",
+        props: ['newbook', 'eidtbook', 'hotbook'],
+        data() {
+            return {
+            };
+        }
+    };
+</script>
+<style lang="less">
+@import "./../styles/booklist.less";
+</style>
