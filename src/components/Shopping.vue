e27ffca2fc261e89e3f006f27fcac38c8f1b5add
51942664 <51942664@qq.com>
2017-12-12 11:31:05 +0800

    新增购物车

---
+++ src/components/Shopping.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="shopping">
+    {{msg}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Shopping',
+  data () {
+    return {
+      msg: 'shopping'
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
