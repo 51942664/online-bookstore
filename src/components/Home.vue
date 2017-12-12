ada7a01b5a1bb64dcc7b9012b098272cc1fe9002
51942664 <51942664@qq.com>
2017-12-12 11:30:05 +0800

    新增主页

---
+++ src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    {{msg}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      msg: 'home'
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
