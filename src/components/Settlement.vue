20729c436de30dde90c6b72c3996560e848e90e2
z34xuan <18723688919@163.com>
2017-12-13 14:33:09 +0800

    增加了结算中心文件

---
+++ src/components/Settlement.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="settlement">
+    <header>
+      <h3>结算中心</h3>
+    </header>
+    <main>
+      <h1>商品信息确认</h1>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Settlement',
+  data () {
+    return {
+      msg: 'settlement'
+    }
+  }
+}
+</script>
+
+<style scoped lang="less">
+  @import "../styles/Settlement.less";
+</style>
