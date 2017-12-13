24c77ad13f1178cb96049f65975ac4b5726af035
z34xuan <18723688919@163.com>
2017-12-13 14:57:36 +0800

    增加结算中心内容

--- src/components/Settlement.vue
+++ src/components/Settlement.vue
@@ -5,6 +5,11 @@
     </header>
     <main>
       <h1>商品信息确认</h1>
+      <section class="infor-confirm">
+        <div>
+          <h2>价格信息</h2>
+        </div>
+      </section>
     </main>
   </div>
 </template>
