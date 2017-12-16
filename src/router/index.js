330012e0f6e226b4bb5be779abac4d10cb8cc7b3
51942664 <51942664@qq.com>
2017-12-16 19:11:38 +0800

    修改了全局组件

--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,6 @@ import Self from '@/components/Self'
 
 import Settlement from '@/components/Settlement'
 
-import ReceAddress from '@/components/Rece-address'
 import PerData from '@/components/PerData'
 
 import Address from '@/components/Address'
@@ -44,11 +43,6 @@ export default new Router({
       path: '/settlement',
       name: 'settlement',
       component: Settlement
-    },
-    {
-       path: '/rece-address',
-      name: 'rece-address',
-      component: ReceAddress
     },
     {
       path: '/perData',
