ce585b04424ba1a9a3a8c462ad14a03d4c887aed
xutaiyun1994 <1136118852@qq.com>
2017-12-17 17:28:44 +0800

    修改了个人中心

--- src/components/Describe.vue
+++ src/components/Describe.vue
@@ -47,14 +47,8 @@
         </div>
         <!-- btn -->
         <div class="btn">
-            <button type="button" @click="addShopCar">加入购物车</button>
-            <router-link :to="{name:'settlement'}" tag="a">
-                <button type="button" class="active" @click="purchase">立即购买</button>
-            </router-link>
-        </div>
-        <!-- 遮罩 -->
-        <div v-if="showMask" class="mask">
-            <div>{{buy}}</div>
+            <button type="button">加入购物车</button>
+            <button type="button" class="active">立即购买</button>
         </div>
     </div>
 </template>
@@ -64,75 +58,19 @@
         props: ["describe", "author", "page", "booknum", "date", "test"],
         data() {
             return {
-                getDescribe: {},
-                buy: "已添加成功，在购物车等亲",
-                showMask: false,
-                describeObj: {}
+                getDescribe: {}
             };
         },
         methods: {
-            getDescribeObj() {
+            getDescribeObj(){
                 this.getDescribe = this.$props.describe;
                 console.log(this.getDescribe)
-            },
-            // 本地缓存，让购物车获取数据
-            add(key, infos) {
-                var Data = null;
-                if (localStorage[key]) {
-                    Data = JSON.parse(localStorage[key]);
-                } else {
-                    Data = [];
-                }
-                Data.push(infos);
-                localStorage[key] = JSON.stringify(Data);
-            },
-            addShopCar() {
-                var $this = this;
-                this.showMask = !this.showMask;
-                // 存放数据;
-                this.describeObj = {
-                    img: this.$props.describe.img,
-                    name: this.$props.describe.name,
-                    author: this.$props.describe.author,
-                    price: this.$props.describe.price
-                };
-                // 判断本地缓存是否已经有值;
-                if (!localStorage.getItem('describe')) {
-                    this.add('describe', this.describeObj);
-                } else {
-                    var name = this.describeObj.name
-                    var getdescrube = JSON.parse(localStorage.getItem('describe')),
-                        getdescrubeLength = getdescrube.length;
-                    for (var i = 0; i < getdescrubeLength; i++) {
-                        if (name === getdescrube[i].name) {
-                              setTimeout(function () {
-                                $this.buy = "亲，已经在购物车，请勿重复添加哦";
-                                return;
-                              })
-                        } else {
-                            this.add('describe', this.describeObj);
-                        }
-                    }
-                }
-                setTimeout(function () {
-                    $this.showMask = !$this.showMask;
-                }, 2000);
-            },
-            purchase() {
-                this.describeObj = {
-                    img: this.$props.describe.img,
-                    name: this.$props.describe.name,
-                    author: this.$props.describe.author,
-                    price: this.$props.describe.price
-                };
-                var describe = JSON.stringify(this.describeObj)
-                localStorage.setItem('buy', describe);
             }
         },
-        mounted() {
+        mounted(){
             setTimeout(() => {
                 this.getDescribeObj();
-            }, 1000);
+            },1000);
         }
     }
 
